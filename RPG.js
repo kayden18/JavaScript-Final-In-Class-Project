ab390ca7d6c7b70704a65ca1a7701e079f9cb377
Kayden Williams <10789755@AD.UVU.EDU>
2016-10-12 11:49:41 -0600

    snip

--- RPG.js
+++ RPG.js
@@ -15,5 +15,29 @@ alert("Its extremly quiet. There's a door and a window.");
 
 var escape = prompt ("Which exit do you choose?");
 
-prompt("You go to the " + escape + " finding it locked. Do you search for a lockpick or try the other exit?");
+alert("You go to the " + escape + " finding it locked. You attempt to pick the lock.");
 
+var lockpickSkill = Math.round(Math.random() * 1);
+
+var beginningScenarios = [""];
+
+function randomNumber( range ) {
+    return Math.round( Math.random( ) * range );
+}
+
+alert( beginningScenarios[ randomNumber(beginningScenarios.length - 1 ) ] );
+
+    var weaponList = ["axe", "crossbow", "chainsaw", "katana", "rubber chicken"];
+    
+    var weapon = weaponList[ randomNumber ( weaponList.length - 1 ) ];
+    alert("You escape the cell and hear a loud noise further down the hall. your search around for a" + weapon + ".");
+
+alert("Suddenly a zombie bursts through the door! You ready your " + weapon + " and advance towards the zombie.");
+
+var survival = randomNumber(2)
+
+if (survival === 0){
+    alert("The zombie bites you. You lose! ");
+} else if ( survival > 0){
+    alert("You kill the zombie with your " + weapon + ". You win!")
+}
