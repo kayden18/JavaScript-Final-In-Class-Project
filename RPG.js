5ea2615619660ac50b61bd6e80275d8162634eed
Kayden Williams <10789755@AD.UVU.EDU>
2016-10-14 11:49:07 -0600

    more material

--- RPG.js
+++ RPG.js
@@ -1,43 +1,75 @@
 //setting prison
 //races Orc, Elf, Human
 //weapons Crossbow, Greatsword, Warhammer, Twin Daggers
-//Items Heath Potion, gold, reviver
 
-alert("You awake in a jail cell, with a pounding in the back of your skull. You glance around to get your barings before getting to your feet.");
+alert("You awake in a jail cell, with a pounding in the back of your skull. You glance around to get your barings before getting to your feet."); 
+ 
+var playerRace = prompt("Choose a Race: Orc, Elf, or Human."); 
+console.log(playerRace); 
+ 
+var playerName = prompt ("What is your name?"); 
+console.log(playerName); 
+ 
+var armorList = ["light", "medium", "Heavy"];
 
-var playerRace = prompt("Choose a Race: Orc, Elf, or Human.");
-console.log(playerRace);
+var Hero = {
+    weapon: "none",
+    strength: 5,
+    armor: "none",
 
-var playerName = prompt ("What is your name?");
-console.log(playerName);
+    UpgradeArmor: function(change){
+        this.armor = change;
+    },
 
-alert("Its extremly quiet. There's a door and a window.");
-
-var escape = prompt ("Which exit do you choose?");
-
-alert("You go to the " + escape + " finding it locked. You attempt to pick the lock.");
-
-var lockpickSkill = Math.round(Math.random() * 1);
-
-var beginningScenarios = [""];
-
-function randomNumber( range ) {
-    return Math.round( Math.random( ) * range );
 }
 
-alert( beginningScenarios[ randomNumber(beginningScenarios.length - 1 ) ] );
+alert("Its extremly quiet. There's a door and a window. You go to the door finding it unlocked. Outside the cell is a chest you open it to find some weapons. A Crossbow, a Greatsword, a Warhammer, and Twin Daggers."); 
+var weapon = prompt("Choose your weapon"); 
 
-    var weaponList = ["axe", "crossbow", "chainsaw", "katana", "rubber chicken"];
-    
-    var weapon = weaponList[ randomNumber ( weaponList.length - 1 ) ];
-    alert("You escape the cell and hear a loud noise further down the hall. your search around for a" + weapon + ".");
+var survival = randomNumber()
 
-alert("Suddenly a zombie bursts through the door! You ready your " + weapon + " and advance towards the zombie.");
+alert("You examine your new weapon and hear a loud noise further down the hall. You ready your " + weapon + ". Suddenly a zombie came running down the hall."); 
+function randomNumber(survival){
+     Math.random(2); 
 
-var survival = randomNumber(2)
+if (survival === 0){ 
+    alert("You dodge the attack and continue through the jail."); 
+} else if ( survival > 0){ 
+    alert("You kill the enemy and continue your way through the jail.") 
+}
+alert("You pick up scraps of cloth and metal before you reach the exit and you can now make armor.")
+Hero.UpgradeArmor(prompt("What kind of armor will you make light, medium or heavy?"));
+alert(playerName + " now has " + Hero.armor + " armor.");
 
-if (survival === 0){
-    alert("The zombie bites you. You lose! ");
-} else if ( survival > 0){
-    alert("You kill the zombie with your " + weapon + ". You win!")
+alert("You leave the jail and step out into the street. You notice a small goblin looking through some trash. Of course you attack it for the exp. Your attack hits but the goblin lived and strikes back.")
+
+if(survival === 0){
+    alert("Goblin lands a hit dropping your health to 10.")
+} else if (survival > 0){
+    alert("You dodge the attack and ready for your next attack.")
+}
+
+if(survival === 0){
+    alert("You attack the goblin again killing it.")
+} else if (survival > 0){
+    alert("You decide to run from the goblin.")
+}
+
+alert("Catching your breath you sense a something sneaking up behind you.")
+
+if(survival === 0){
+    alert("You turn and counter the attacker.")
+} else if (survival > 0){
+    alert("You take a direct hit to your back.")
+}
+
+alert("Trying to get a hold of the situation you examine the enemy. It appears to be demon.")
+
+if(survival === 0){
+    alert("You unleash your a final attack and kill the demon.")
+} else if (survival > 0){
+    alert("The demon grabs you and places a gem to your forehead teleporting you to the demon realm.")
+}
+
+alert("To Be Continued...")
 }
